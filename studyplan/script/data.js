3d2f819fb0fb4931fdb1c3411aa2224394b0ae10
pkt247 <pkt75632@gmail.com>
2024-09-15 23:20:35 +0530

    modified:   studyplan/script/data.js

--- studyplan/script/data.js
+++ studyplan/script/data.js
@@ -1,140 +1,139 @@
 const w1 = [
-    "The Living World",
-    "Units and Measurement",
-    "Some Basic Concepts of Chemistry"
-  ];
-  
-  const w2 = [
-    "Biological Classification",
-    "Motion in Straight Line",
-    "Vectors"
-  ];
-  
-  const w3 = [
-    "Plant Kingdom",
-    "Motion in Plane",
-    "Structure of Atom"
-  ];
-  
-  const w4 = [
-    "Cell: The Unit of Life",
-    "Cell Cycle and Cell Division",
-    "Animal Kingdom"
-  ];
-  
-  // Phase 2 Weeks
-  const w5_6 = [
-    "Sexual Reproduction in Flowering Plants",
-    "Microbes in Human Welfare",
-    "Ecosystem",
-    "Human Reproduction",
-    "Reproductive Health",
-    "Photosynthesis in Higher Plants",
-  ];
-  
-  const w7_8 = [
-    "Respiration in Plants",
-    "Plant Growth and Development",
-    "Molecular Basis of Inheritance",
-    "Biotechnology and its Applications",
-    "Biotechnology- Principles and Processes ",
-    "Morphology of Flowering Plants",
-    "Anatomy of Flowering Plants",
-  ];
-  
-  const w9_10 = [
-    "Principles of Inheritance and Variation",
-    "Biodiversity and its Conservation",
-    "Organisms and Populations",
-    "Evolution",
-    "Human Health and Diseases",
-    "Biomolecule (Biology)",
-    
-  ];
-  
-  const w11_12 = [
-    "Structural Organisation in Animals",
-    "Locomotion and Movement",
-    "Neural Control and Coordination",
-    "Chemical Coordination & Integration",
-    "Breathing and Exchange of Gases",
-    "Body Fluids and Circulation",
-    "Excretory Products and their Elimination",
-  ];
-  
-  // Phase 3 Weeks
-  const w13_17 = [
-    "Classification of Elements and Periodicity in Properties",
-    "Chemical Bonding and Molecular Structure",
-    "Thermodynamics (chemistry)",
-    "Redox Reactions",
-    "Equilibrium",
-    "Amines",
-    "p-block Elements",
-    "The d-and f-Block Elements",
-  ];
-  
-  const w18_22 = [
-    "Mechanical Properties of Solids",
-    "Mechanical Properties of Fluids",
-    "Thermal Properties of Matter",
-    "Kinetic Theory of Gases",
-    "Organic Chemistry: Some Basic Principles and Techniques",
-    "Biomolecule (chemistry)",
-    "Hydrocarbons",
-    "Haloalkanes and Haloarenes",
-    
-  ];
-  
-  const w23_27 = [
-    "Alcohols, Phenols and Ethers",
-    "Aldehydes, Ketones and Carboxylic Acids",
-    "Chemical Kinetics",
-    "Atoms ",
-    "Nuclei",
-    "Semiconductor Electronics",
-    "Solutions",
-    "Dual Nature of Radiation and Matter",
-  ];
-  
-  const w28_32 = [
-    
-    "Laws of Motion ",
-    "Work, Energy, and Power ",
-    "System of Particle and Rotational Motion ",
-    "Electric Charges and Fields",
-    "Electrostatic Potential and Capacitance",
-    "Current Electricity",
-    "Oscillations",
-    "Thermodynamics (physics)",
-    
-  ];
-  
-  const w33_37 = [
-    "Moving charges and Magnetism",
-    "Magnetism and Matter",
-    "Electromagnetic Induction",
-    "Alternating Currents",
-    "Electromagnetic Waves",
-    "Gravitation",
-    "Waves",
-    "Ray Optics and Optical Instruments",
-    "Wave Optics",
-  ];
-  
-  // Phase 4 Weeks
-  const w38_42 = [
-    "Revise all subjects systematically, focusing on weak areas and important topics.",
-    "Solve PYQs to assess preparation level and improve time management skills."
-  ];
-  
-  const w43_45 = [
-    "Do lite revision again of all topics"
-  ];
-  
-  // Study Plan Phases
-  const p1 = [w1, w2, w3, w4];
-  const p2 = [w5_6, w7_8, w9_10, w11_12];
-  const p3 = [w13_17, w18_22, w23_27, w28_32, w33_37];
-  const p4 = [w38_42, w43_45];
-  
+  "The Living World",
+  "Units and Measurement",
+  "Some Basic Concepts of Chemistry"
+];
+
+const w2 = [
+  "Vectors",
+  "Biological Classification",
+  "Motion in Straight Line",
+];
+
+const w3 = [
+  "Plant Kingdom",
+  "Motion in Plane",
+  "Structure of Atom"
+];
+
+const w4 = [
+  "Cell: The Unit of Life",
+  "Cell Cycle and Cell Division",
+  "Animal Kingdom"
+];
+
+// Phase 2 Weeks
+const w5_6 = [
+  "Sexual Reproduction in Flowering Plants",
+  "Microbes in Human Welfare",
+  "Ecosystem",
+  "Human Reproduction",
+  "Reproductive Health",
+  "Photosynthesis in Higher Plants",
+];
+
+const w7_8 = [
+  "Respiration in Plants",
+  "Plant Growth and Development",
+  "Molecular Basis of Inheritance",
+  "Biotechnology and its Applications",
+  "Biotechnology- Principles and Processes ",
+  "Morphology of Flowering Plants",
+  "Anatomy of Flowering Plants",
+];
+
+const w9_10 = [
+  "Principles of Inheritance and Variation",
+  "Biodiversity and its Conservation",
+  "Organisms and Populations",
+  "Human Health and Diseases",
+  "Biomolecule (Biology)",
+  "Evolution",
+
+];
+
+const w11_12 = [
+  "Structural Organisation in Animals",
+  "Locomotion and Movement",
+  "Neural Control and Coordination",
+  "Chemical Coordination & Integration",
+  "Breathing and Exchange of Gases",
+  "Body Fluids and Circulation",
+  "Excretory Products and their Elimination",
+];
+
+// Phase 3 Weeks
+const w13_17 = [
+  "Classification of Elements and Periodicity in Properties",
+  "Chemical Bonding and Molecular Structure",
+  "Redox Reactions",
+  "Mechanical Properties of Solids",
+  "Mechanical Properties of Fluids",
+  "Thermal Properties of Matter",
+  "Kinetic Theory of Gases",
+  "Thermodynamics (chemistry)",
+];
+
+const w18_22 = [
+  "Equilibrium",
+  "p-block Elements",
+  "The d-and f-Block Elements",
+  "Organic Chemistry: Some Basic Principles and Techniques",
+  "Hydrocarbons",
+  "Haloalkanes and Haloarenes",
+  "Alcohols, Phenols and Ethers",
+  "Aldehydes, Ketones and Carboxylic Acids",
+
+];
+
+const w23_27 = [
+  "Biomolecule (chemistry)",
+  "Amines",
+  "Chemical Kinetics",
+  "Atoms ",
+  "Nuclei",
+  "Semiconductor Electronics",
+  "Solutions",
+  "Dual Nature of Radiation and Matter",
+];
+
+const w28_32 = [
+
+  "Laws of Motion ",
+  "Work, Energy, and Power ",
+  "System of Particle and Rotational Motion ",
+  "Electric Charges and Fields",
+  "Electrostatic Potential and Capacitance",
+  "Current Electricity",
+  "Oscillations",
+  "Thermodynamics (physics)",
+
+];
+
+const w33_37 = [
+  "Moving charges and Magnetism",
+  "Magnetism and Matter",
+  "Electromagnetic Induction",
+  "Alternating Currents",
+  "Electromagnetic Waves",
+  "Gravitation",
+  "Waves",
+  "Ray Optics and Optical Instruments",
+  "Wave Optics",
+];
+
+// Phase 4 Weeks
+const w38_42 = [
+  "Revise all subjects systematically, focusing on weak areas and important topics.",
+  "Solve PYQs to assess preparation level and improve time management skills."
+];
+
+const w43_45 = [
+  "Do lite revision again of all topics"
+];
+
+// Study Plan Phases
+const p1 = [w1, w2, w3, w4];
+const p2 = [w5_6, w7_8, w9_10, w11_12];
+const p3 = [w13_17, w18_22, w23_27, w28_32, w33_37];
+const p4 = [w38_42, w43_45];
